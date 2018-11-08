be73ba0cf11581180fc2a68a9c493b95b9830e18
jcowen-Berklee <jcowen@berklee.edu>
2018-11-07 20:10:37 -0500

    Creating Collectable Objects
    The end state of the above mentioned tutorial video.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿//Unity 2D UFO Tutorial from 
+//https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial
+//updated to Unity 2018.2
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);	
+	}
+}
