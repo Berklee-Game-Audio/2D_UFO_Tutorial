fca890c38c2433a6c24268a4f83b21d8269e8b59
jcowen-Berklee <jcowen@berklee.edu>
2018-11-07 21:04:38 -0500

    Counting Collectables and Displaying Score
    This represent the ending state of the Counting and Score tutorial video.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//add the Canvas namespace
+using UnityEngine.UI;
+
 public class PlayerController : MonoBehaviour {
 
 	//used to control the player's speed
 	public float speed;
 
+	//use a public variable to display the count
+	public Text countText;
+
+	public Text winText;
+
 	//we need a Rigidbody for the physics system to work with
 	private Rigidbody2D rb2d;
 
+	//a place to hold the number of pickups we have triggered
+	private int count;
 
 	private void Start()
 	{
 		//we associate the component on the current object to the Rigidbody2D variable
 		rb2d = GetComponent<Rigidbody2D>();
+
+		//initialize count to zero when the scene is loaded
+		count = 0;
+
+		//set a blank message to start the game
+		winText.text = " ";
+
+		//set the text display to the current value of count
+		SetCountText();
 	}
 
 	//please see the decription for FixedUpdate from the Unity manual at
@@ -45,6 +64,26 @@ public class PlayerController : MonoBehaviour {
 		{
 			//disable the pickups once they are collided with
 			other.gameObject.SetActive (false);
+
+			//increment the count variable
+			count = count + 1;
+
+			//update the Counttext UI element
+			SetCountText();		
 		}
+	}
+
+	void SetCountText () 
+	{
+		//send the count value to the displayed text UI element
+		countText.text = "Count: " + count.ToString();
+
+		//check to see if all of the pickups have been collected
+		//if they have, display a message
+		if (count >= 12)
+		{
+			winText.text = "You Win!!!";
+		}
+	
 	}
 }
